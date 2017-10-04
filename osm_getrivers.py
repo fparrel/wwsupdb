17762ae3faca94ac07a3217b7acd1fa2ea1204bd
Frederic PARREL <fredericparrel@gmail.com>
2017-10-04 16:15:51 +0100

    Initial commit

---
+++ osm_getrivers.py
@@ -0,0 +1,45 @@
+import requests
+import re
+import json
+
+RIVERREGEX = re.compile('<a target="map" href="queryinmap.php\?BBOX=[0-9\.,\+]+&name=([A-Za-z0-9%%+-]+)&key=waterway&value=river&types=lines">(.*)</a>')
+#RIVERREGEX = re.compile('target="(.*)"')
+LINESTRINGREGEX = re.compile('<LineString><coordinates>(.*)</coordinates>')
+
+def get_rivers(bbox):
+    # Get list of rivers on bbox
+    r = requests.get('http://tools.wmflabs.org/query2map/index.php?key=waterway&value=river&types=lines&BBOX=%f%%2C%f%%2C%f%%2C%f'%bbox)
+    t = r.text.encode(r.encoding)
+    for river in RIVERREGEX.findall(t):
+        print river
+        # Get path of river
+        r = requests.get('http://tools.wmflabs.org/query2map/osm-to-kml.php?BBOX=%f,%f,%f,%f&name=%s&key=waterway&value=river&types=lines'%(bbox[0],bbox[1],bbox[2],bbox[3],river[0]))
+        t = r.text
+        lines = LINESTRINGREGEX.findall(t)
+        print len(lines)
+        paths = map(lambda linestrsplitted: map(lambda xvy:map(float,xvy.split(',')),linestrsplitted),map(lambda linestr: linestr.split(' '),lines))
+        #line = map(lambda xvy:map(float,xvy.split(',')),map(lambda linestr: linestr.split(' '),lines))
+        yield {'_id':river[1],'paths':paths}
+
+
+
+def main():
+    rivers = json.load(open('osm_rivers_06.json','r'))
+    for river in rivers:
+        if river['_id']=='La Brague':
+            for i in range(0,len(river['paths'])):
+                for j in range(0,len(river['paths'])):
+                    if i!=j and len(river['paths'][i])>0 and len(river['paths'][j])>0 and river['paths'][j][0] == river['paths'][i][-1]:
+                        river['paths'][i] += river['paths'][j]
+                        river['paths'][j] = []
+            paths = filter(lambda path:len(path)>0,river['paths'])
+            print len(river['paths']),len(paths)
+    exit()
+    
+    dept06_fr = (6.559029,43.455733,7.734566,44.363941)
+    france = (-7.84,42.27,8.25,51.18)
+    bbox = dept06_fr
+    json.dump(list(get_rivers(bbox)),open('osm_rivers_06.json','w'))
+
+if __name__=='__main__':
+    main()
