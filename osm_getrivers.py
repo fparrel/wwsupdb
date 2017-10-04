75752617db87f08cc94b7e66c9033c5413bec29b
Frederic PARREL <frederic.parrel@amadeus.com>
2017-10-04 16:40:08 +0100

    Fix appending algo

--- osm_getrivers.py
+++ osm_getrivers.py
@@ -26,20 +26,26 @@ def get_rivers(bbox):
 def main():
     rivers = json.load(open('osm_rivers_06.json','r'))
     for river in rivers:
-        if river['_id']=='La Brague':
-            for i in range(0,len(river['paths'])):
-                for j in range(0,len(river['paths'])):
-                    if i!=j and len(river['paths'][i])>0 and len(river['paths'][j])>0 and river['paths'][j][0] == river['paths'][i][-1]:
-                        river['paths'][i] += river['paths'][j]
-                        river['paths'][j] = []
-            paths = filter(lambda path:len(path)>0,river['paths'])
-            print len(river['paths']),len(paths)
+        for i in range(0,len(river['paths'])):
+            for j in range(0,len(river['paths'])):
+                if i!=j and len(river['paths'][i])>0 and len(river['paths'][j])>0 and river['paths'][j][0] == river['paths'][i][-1]:
+                    river['paths'][i] += river['paths'][j]
+                    river['paths'][j] = []
+                elif i!=j and len(river['paths'][i])>0 and len(river['paths'][j])>0 and river['paths'][i][0] == river['paths'][j][-1]:
+                    river['paths'][j] += river['paths'][i]
+                    river['paths'][i] = []
+        paths = filter(lambda path:len(path)>0,river['paths'])
+        assert(sum(map(len,river['paths']))==sum(map(len,paths)))
+        river['paths']=paths
+    json.dump(rivers,open('osm_rivers_06-2.json','w'))
     exit()
     
     dept06_fr = (6.559029,43.455733,7.734566,44.363941)
     france = (-7.84,42.27,8.25,51.18)
     bbox = dept06_fr
-    json.dump(list(get_rivers(bbox)),open('osm_rivers_06.json','w'))
+    json.dump(list(get_rivers(bbox)),open('osm_rivers_06_tmp.json','w'))
+    #bbox = france
+    #json.dump(list(get_rivers(bbox)),open('osm_rivers_fr.json','w'))
 
 if __name__=='__main__':
     main()
