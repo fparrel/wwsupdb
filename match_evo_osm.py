43c3785fdefe894212dbbb9bbde9f576f0978836
Frederic Parrel <fredericparrel@gmail.com>
2017-10-12 17:39:08 +0200

    insignificant tokens ignored

--- match_evo_osm.py
+++ match_evo_osm.py
@@ -1,5 +1,16 @@
+# -*- coding: utf8 -*-
+
 import json
 from fuzzywuzzy import fuzz
+
+def remove_tokens(s,tokens):
+    for token in tokens:
+        i = 0
+        while i>-1:
+            i = unicode(s).find(unicode(token,encoding='utf8'))
+            if i>-1:
+                s=s[:i]+s[i+len(token):]
+    return s
 
 console_encoding = 'utf8' #latin1 on Windows
 
@@ -12,25 +23,27 @@ rivers_output = []
 
 # Try to match
 
+unsignificant_tokens = ['Rivière','Ruisseau']
+
 for river_evo in rivers_evo:
     name_evo = river_evo['name']
     matches = []
 
     # First try to match with partial_ratio (example: 'The Big River" = "Big River")
     for river_osm in rivers_osm:
-        if fuzz.partial_ratio(name_evo,river_osm['_id']) == 100:
+        if fuzz.partial_ratio(name_evo,remove_tokens(river_osm['_id'],unsignificant_tokens)) == 100:
             matches.append((river_osm['_id'],river_osm))
 
     if len(matches)>0:
 
         # If there are several matches use ratio instead of partial_ratio and get the best
         if len(matches)>1:
-            ratio_matches = map(lambda match: (fuzz.ratio(name_evo,match[0]),match),matches)
+            ratio_matches = map(lambda match: (fuzz.ratio(name_evo,remove_tokens(match[0],unsignificant_tokens)),match),matches)
             ratio_matches.sort(reverse=True)
             match = ratio_matches[0][1]
         else:
             match = matches[0]
-        print name_evo.encode(console_encoding), match[1]['_id'].encode(console_encoding)
+        print 'evo: %s\tosm: %s' % (name_evo.encode(console_encoding), match[1]['_id'].encode(console_encoding))
         river_evo['name_osm'] = match[1]['_id']
         river_evo.update(match[1])
         rivers_output.append(river_evo)
--- scrap.sh
+++ scrap.sh
@@ -1,4 +1,5 @@
 rm eauxvives_org.json
 rm eauxvives_org.log
 scrapy runspider eauxvives_org-scrap.py -o eauxvives_org.json --logfile=eauxvives_org.log
-python format_eauxvives_org_json.py
+#python format_eauxvives_org_json.py
+
