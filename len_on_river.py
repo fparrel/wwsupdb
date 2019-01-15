f96cd6a0b575d5e1ebee1370d855257b2acf91b8
Frederic PARREL <fredericparrel@gmail.com>
2019-01-15 17:22:24 +0100

    Option to add pathid on river length computing script

--- len_on_river.py
+++ len_on_river.py
@@ -6,10 +6,13 @@ from geo_utils import len_of_path,len_btw_2pts
 def main():
 
   # Parse arguments or display help
-  if len(sys.argv)==6:
+  pathid = -1
+  if len(sys.argv)>=6:
     name=sys.argv[1]
     a=[float(sys.argv[2].rstrip(",")),float(sys.argv[3])]
     b=[float(sys.argv[4].rstrip(",")),float(sys.argv[5])]
+    if len(sys.argv)==7:
+      pathid = int(sys.argv[6])
   elif len(sys.argv)==2:
     if sys.argv[1] in ('-h','--help'):
       print 'Usage: %s "River name" lat1 lon1 lat2 lon2' % sys.argv[0]
@@ -30,15 +33,19 @@ def main():
   # Get river from db
   river = db.rivers.find_one({"_id":name})
   if river==None:
-    print 'River not found in db'
-    return
+    river = db.osm.find_one({"_id":name})
+    if river==None:
+      print 'River not found in db'
+      return
 
   # Sanity check
-  if not(len(river["paths"])==1):
+  if not(len(river["paths"])==1) and pathid==-1:
     print 'Only implemented for 1 single path rivers'
     return
+  elif pathid==-1:
+    pathid = 0
 
-  path=river["paths"][0]
+  path=river["paths"][pathid]
 
   if a==None:
     # Compute full length of river
@@ -49,3 +56,4 @@ def main():
 
 if __name__=='__main__':
   main()
+
